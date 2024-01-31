3f3ca9efc1ca7a4ca811633ad186c91fd2d8887d
Ricardo <ricardorgvillanueva@gmail.com>
2024-01-31 14:56:49 -0500

    adding a database

--- my-new-app/src/ShipmentStatus.jsx
+++ my-new-app/src/ShipmentStatus.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRobot } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
-
 const ShipmentStatus = () => {
     const [isBotActive, setIsBotActive] = useState(false);
     const [botMessage, setBotMessage] = useState('');
@@ -14,15 +13,15 @@ const ShipmentStatus = () => {
     useEffect(() => {
         // Fetch shipments data from the Express.js API
         axios.get('http://localhost:4000/api/shipments')
-          .then((response) => {
-            setShipments(response.data);
-          })
-          .catch((error) => {
-            console.error('Error fetching shipments data:', error.message);
-          });
-      }, []);
+            .then((response) => {
+                setShipments(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching shipments data:', error.message);
+            });
+    }, []);
 
-    const hasActionNeededOrders = true; 
+    const hasActionNeededOrders = true;
 
     useEffect(() => {
         if (hasActionNeededOrders) {
@@ -33,7 +32,6 @@ const ShipmentStatus = () => {
             setIsBotActive(true);
         }
     }, [hasActionNeededOrders]);
-
 
     const handleDrawerOpen = () => {
         setIsOpen(true);
@@ -54,41 +52,42 @@ const ShipmentStatus = () => {
                 </DrawerContent>
             </Drawer>
 
-            <Heading as="h1" size="xl" textAlign="center" my={6} width="100vw">
+            <Heading as="h1" size="xl" textAlign="center" my={6} width="100vw" color="navy">
                 Shipment Status
             </Heading>
 
-            <Table variant="simple" width="100vw">
-                <Thead>
-                    <Tr>
-                        <Th>PO Number</Th>
-                        <Th>Client</Th>
-                        <Th>Product</Th>
-                        <Th>ETA</Th>
-                        <Th>Status</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {Object.keys(shipments).map((shipmentKey) => {
-                        const shipment = shipments[shipmentKey];
-                        return (
-                            <Tr key={shipmentKey}>
-                                <Td>{shipment.poNumber}</Td>
-                                <Td>{shipment.client}</Td>
-                                <Td>{shipment.product}</Td>
-                                <Td>{shipment.eta}</Td>
-                                <Td>{shipment.status}</Td>
-                            </Tr>
-                        );
-                    })}
-                </Tbody>
-            </Table>
+            <Table variant="simple" width="100vw" colorScheme="blue" borderWidth="1px" borderRadius="md" boxShadow="md">
+    <Thead>
+        <Tr>
+            <Th color="white" bg="navy" py={2} fontSize="lg">PO Number</Th>
+            <Th color="white" bg="navy" py={2} fontSize="lg">Client</Th>
+            <Th color="white" bg="navy" py={2} fontSize="lg">Product</Th>
+            <Th color="white" bg="navy" py={2} fontSize="lg">ETA</Th>
+            <Th color="white" bg="navy" py={2} fontSize="lg">Status</Th>
+        </Tr>
+    </Thead>
+    <Tbody>
+        {Object.keys(shipments).map((shipmentKey) => {
+            const shipment = shipments[shipmentKey];
+            return (
+                <Tr key={shipmentKey}>
+                    <Td>{shipment.poNumber}</Td>
+                    <Td>{shipment.client}</Td>
+                    <Td>{shipment.product}</Td>
+                    <Td>{shipment.eta}</Td>
+                    <Td>{shipment.status}</Td>
+                </Tr>
+            );
+        })}
+    </Tbody>
+</Table>
+
 
             {isBotActive && (
-                <Box position="fixed" top="0px" right="20px" p="4" bg="white" borderRadius="md" boxShadow="md" width="225px">
-                    <Flex alignItems="center"> 
-                        <FontAwesomeIcon icon={faRobot} style={{ marginRight: '10px' }}/>
-                        <Text>{botMessage}</Text>
+                <Box position="fixed" top="20px" right="20px" p="4" bg="navy" borderRadius="md" boxShadow="md" width="225px">
+                    <Flex alignItems="center">
+                        <FontAwesomeIcon icon={faRobot} style={{ marginRight: '10px' }} color="white" />
+                        <Text color="white">{botMessage}</Text>
                     </Flex>
                 </Box>
             )}
---
+++ my-new-app/src/shipments.js
@@ -0,0 +1,233 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const locateChrome = require("chrome-location");
+puppeteer.use(StealthPlugin());
+
+const shipments = {
+  shipment1: {
+    client: "Interandina",
+    steamshipLine: "MSC",
+    poNumber: "5023-165K",
+    product: "wine",
+    supplier: "Cargill",
+    container: "CRSU6024460",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+  shipment2: {
+    client: "Diser",
+    steamshipLine: "MSC",
+    poNumber: "5023-162K",
+    product: "wine",
+    supplier: "Cargill",
+    container: "GESU9357858",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+  shipment3: {
+    client: "Diser",
+    steamshipLine: "Maersk",
+    poNumber: "5023-161K",
+    product: "wine",
+    supplier: "Cargill",
+    container: "SUDU6068809",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+  shipment4: {
+    client: "Interandina",
+    poNumber: "5023-164K",
+    product: "wine",
+    steamshipLine: "Maersk",
+    supplier: "Cargill",
+    container: "MWCU5295851",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+  shipment5: {
+    client: "AW shipments",
+    poNumber: "5023-147K",
+    product: "wine",
+    steamshipLine: "ONE",
+    supplier: "JBS",
+    container: "FSCU5886903",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+  shipment6: {
+    client: " poNumberllopez",
+    poNumber: "5023-48K",
+    product: "wine",
+    steamshipLine: "ONE",
+    supplier: "JBS",
+    container: "SZLU9446927",
+    eta: "2024-02-15",
+    status: "In Transit",
+  },
+};
+
+// Function to track a single shipment
+async function trackShipment(shipment) {
+  let eta;
+  switch (shipment.steamshipLine) {
+    case "MSC":
+      eta = await runMsc(shipment.container);
+      break;
+    // case "Maersk":
+    //   eta = await runMaersk(shipment.container);
+    //   break;
+    // case "ONE":
+    //   eta = await runOne(shipment.container);
+    //   break;
+    default:
+      console.log("Steamship line not supported for tracking");
+      eta = "eta Not available";
+  }
+  shipment.eta = eta;
+  console.log(shipment.eta);
+}
+
+// Function to automate tracking of all shipments
+async function trackShipments() {
+  for (const key in shipments) {
+    if (shipments.hasOwnProperty(key)) {
+      await trackShipment(shipments[key]);
+      await delay(60000); // Wait for 1 minute
+    }
+  }
+}
+
+// Function to introduce a delay to avoid getting flagged
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Run the tracking process
+trackShipments().then(() => console.log("All shipments tracked."));
+
+// Function to help track all MSC shipments
+async function runMsc(containerNumber) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: locateChrome,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.msc.com/en/track-a-shipment?__RequestVerificationToken=token1&trackingMode=1"
+  );
+
+  const searchInputSelector = "#trackingNumber";
+  await page.type(searchInputSelector, containerNumber);
+  await page.keyboard.press("Enter");
+
+  const selector =
+    "body > div.msc-main > div.msc-flow-tracking.separator--bottom-medium > div > div:nth-child(3) > div > div > div > div.msc-flow-tracking__results > div > div > div.msc-flow-tracking__containers > div > div > div.msc-flow-tracking__bar.open > div > div.msc-flow-tracking__cell.msc-flow-tracking__cell--four > div > div > div > span.data-value";
+
+  await page.waitForSelector(selector);
+  const element = await page.$(selector);
+
+  let eta = "Not found";
+  if (element) {
+    eta = await element.evaluate((element) => element.textContent);
+  }
+
+  await browser.close();
+  return eta;
+}
+
+//Function to track ONE shipments
+async function runOne(containerNumber) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: locateChrome,
+  });
+
+  const page = await browser.newPage();
+  const trackingUrl = `https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?redir=Y&ctrack-field=${containerNumber}&sessLocale=en&trakNoParam=${containerNumber}`;
+
+  await page.goto(trackingUrl);
+
+  const iframeSelector = "#IframeCurrentEcom";
+  await page.waitForSelector(iframeSelector);
+  const iframeElement = await page.$(iframeSelector);
+  await page.waitForTimeout(2000);
+  const iframe = await iframeElement.contentFrame();
+
+  const elementInsideIframeSelector =
+    "#detail > tbody > tr:nth-child(10) > td:nth-child(4)";
+  const elementInsideIframe = await iframe.$(elementInsideIframeSelector);
+
+  let eta = "";
+
+  if (elementInsideIframe) {
+    const textContent = await iframe.evaluate(
+      (element) => element.textContent,
+      elementInsideIframe
+    );
+    eta = textContent;
+  } else {
+    console.log(
+      "Element inside iframe not found for container",
+      containerNumber
+    );
+  }
+
+  await browser.close();
+  return eta;
+}
+
+//Function to track Maersk shipments
+async function runMaersk(containerNumber) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: locateChrome,
+  });
+  const page = await browser.newPage();
+  const trackingUrl = `https://www.maersk.com/tracking/${containerNumber}`;
+
+  await page.goto(trackingUrl);
+
+  const selector =
+    "#maersk-app > div > div > div > div.container.container--ocean > dl > dd.container-info__text.container-info__text--date";
+
+  await page.waitForSelector(selector);
+  let element = await page.$(selector);
+
+  if (element) {
+    const text = await page.evaluate((element) => element.textContent, element);
+    element = text;
+  } else {
+    console.log(
+      "Element with selector not found for container",
+      containerNumber
+    );
+  }
+
+  await browser.close();
+  return element;
+}
+
+async function runHapagLloyd(containerNumber) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: locateChrome,
+  });
+
+  const page = await browser.newPage();
+  await page.setBypassCSP(true);
+
+  const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" +
+    Math.floor(Math.random() * 100 + 60) +
+    ".0.0.0 Safari/537.36";
+  await page.setUserAgent(userAgent);
+
+  await page.goto(
+    `https://www.hapag-lloyd.com/en/online-business/track/track-by-container-solution.html?container=${containerNumber}`
+  );
+
+  await browser.close();
+}
+
+module.exports = shipments;
